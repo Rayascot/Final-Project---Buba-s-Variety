092709f4e13a704d8d4785344e934861329515a8
Rea Scott <Thatwonweird1@outlook.com>
2026-01-20 19:15:41 -0500

    The stock matters to the game now and you can lose customers that way. figured out how to stock it from taking stock you don't have and just have to apply that to the rest of the cases

--- Final Project - Buba's Variety/Form1.cs
+++ Final Project - Buba's Variety/Form1.cs
@@ -13,7 +13,7 @@ namespace Final_Project___Buba_s_Variety
         int wantset;
         int UserTotal;
         int score;
-        int NPC;
+        int NPC, daycycle;
         Random monies = new Random();
 
         public Form1()
@@ -144,6 +144,7 @@ namespace Final_Project___Buba_s_Variety
                 requestItems.Text = "I want";
                 coinManagement();
                 NPCEnter();
+                NPC += 1;
                 Random chaos = new Random();
                 int chaosnum = chaos.Next(1, 11);
                 if (chaosnum == 5)
@@ -175,44 +176,155 @@ namespace Final_Project___Buba_s_Variety
                     if (chow >=2 || treatgiver >=1)
                     {
                         chow -= 2;
-                        chowOutput.Text = $"Chow X{chow}";
+                        treatgiver -= 2;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        treatgiverOutput.Text = $"Treatgiver: X{treatgiver}";
+                    } else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                        
                     }
-                    break;
+                        break;
                 case (2):
                     requestItems.Text += "\n" + "Raincoat X1" + "\n" + "Malk X2" + "\n" + "Plushie X1";
-
-                    break;
+                    if (raincoat >= 1 || malk >= 2 || plushies >= 1)
+                    {
+                        raincoat -= 1;
+                        malk -= 2;
+                        plushies -= 1;
+                        RaincoatOutput.Text = $"Raincoat: X{raincoat}";
+                        malkOutput.Text = $"Malk: X{malk}";
+                        plushiesOutput.Text = $"Plushies: X{plushies}";
+                    }else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                        
+                    }
+                        break;
                 case (3):
                     requestItems.Text += "\n" + "Chow X3" + "\n" + "Plushie X2";
-
-                    break;
+                    if (chow >=3|| plushies>=2)
+                    {
+                        chow -= 3;
+                        plushies -= 2;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        plushiesOutput.Text = $"Plushies: X{plushies}";
+                    } else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                       
+                    }
+                        break;
                 case (4):
                     requestItems.Text += "\n" + "Chow X1" + "\n" + "Malk X2" + "\n" + "Plushie X1";
-
-                    break;
+                    if (chow >=1|| malk>=2|| plushies>=1)
+                    {
+                        chow -= 1;
+                        malk -= 2;
+                        plushies -= 1;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        plushiesOutput.Text = $"Plushies: X{plushies}";
+                        malkOutput.Text = $"Malk: X{malk}";
+                    } else if (chow<1|| malk<2|| plushies<1)
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                        
+                    }
+                        break;
                 case (5):
                     requestItems.Text += "\n" + "Treatgiver X2";
-
-                    break;
+                    if (treatgiver>=2)
+                    {
+                        treatgiver -= 2;
+                        treatgiverOutput.Text = $"Treatgiver: X{treatgiver}";
+                    } else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                       
+                    }
+                        break;
                 case (6):
                     requestItems.Text += "\n" + "Chow X2" + "\n" + "Raincoat X1";
-
-                    break;
+                    if (chow >=2|| raincoat>=1)
+                    {
+                        chow -= 2;
+                        raincoat -= 1;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        RaincoatOutput.Text = $"Raincoat: X{raincoat}";
+                    } else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                       
+                    }
+                        break;
                 case (7):
                     requestItems.Text += "\n" + "Chow X2" + "\n" + "Raincoat X2" + "\n" + "Malk X4" + "\n" + "Plushie X1";
+                    if (chow>=2|| raincoat>=2|| malk >=4|| plushies>=1)
+                    {
+                        chow -= 2;
+                        raincoat -= 2;
+                        malk -= 4;
+                        plushies -= 1;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        plushiesOutput.Text = $"Plushies: X{plushies}";
+                        malkOutput.Text = $"Malk: X{malk}";
+                        RaincoatOutput.Text = $"Raincoat: X{raincoat}";
+                    } else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
 
-                    break;
+                    }
+                        break;
                 case (8):
                     requestItems.Text += "\n" + "Chow X4" + "\n" + "Malk x1";
-
-                    break;
+                    if (chow >=4|| malk>=1)
+                    {
+                        chow -= 4;
+                        malk -= 1;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        malkOutput.Text = $"Malk: X{malk}";
+                    }else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                       
+                    }
+                        break;
                 case (9):
                     requestItems.Text += "\n" + "Chow X1" + "\n" + "Raincoat X1" + "\n" + "Treatgiver X1";
-
-                    break;
+                    if (chow>=1||raincoat>=1||treatgiver>=1)
+                    {
+                        chow -= 1;
+                        raincoat -= 1;
+                        treatgiver -= 1;
+                        chowOutput.Text = $"Chow: X{chow}";
+                        treatgiverOutput.Text = $"Treatgiver: X{treatgiver}";
+                        RaincoatOutput.Text = $"Raincoat: X{raincoat}";
+                    }else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                        
+                    }
+                        break;
                 case (10):
                     requestItems.Text += "\n" + "Chow X1" + "\n" + "Raincoat X1" + "\n" + "Malk X1" + "\n" + "Plushies X1" + "\n" + "Treatgiver X1";
-
+                    if (chow >= 1 || raincoat >= 1 || treatgiver >= 1||malk >=1||plushies>=1)
+                    {
+                        chow -= 1;
+                        raincoat -= 1;
+                        treatgiver -= 1;
+                        malk -= 1;
+                        plushies -= 1;
+                        plushiesOutput.Text = $"Plushies: X{plushies}";
+                        malkOutput.Text = $"Malk: X{malk}";
+                        chowOutput.Text = $"Chow: X{chow}";
+                        treatgiverOutput.Text = $"Treatgiver: X{treatgiver}";
+                        RaincoatOutput.Text = $"Raincoat: X{raincoat}";
+                    }
+                    else
+                    {
+                        requestItems.Text = "Uh oh! you didn't stock enough! The customer is unhappy now :(";
+                      
+                    }
                     break;
             }
 
@@ -223,6 +335,7 @@ namespace Final_Project___Buba_s_Variety
             eventPeriodLabel.Text = "Stocking period:Deactive";
             NPCEnter();
             CurrentCustomer.Enabled = true;
+            NPC += 1;
             
         }
 
@@ -281,6 +394,7 @@ namespace Final_Project___Buba_s_Variety
 
         private void coinOutput_Click(object sender, EventArgs e)
         {
+
 
         }
 
